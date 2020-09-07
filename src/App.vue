f6905a764e8d89b5963f92ec8fbb069e96447ff3
danushka96 <danushkaherath96@gmail.com>
2020-09-07 15:30:45 +0530

    customized view

--- src/App.vue
+++ src/App.vue
@@ -10,30 +10,30 @@
             alt="Vuetify Logo"
             class="shrink mr-2"
             contain
-            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+            src="https://i.ibb.co/SsNjKXM/Capture-removebg-preview.png"
             transition="scale-transition"
-            width="40"
+            width="160"
         />
 
-        <v-img
-            alt="Vuetify Name"
-            class="shrink mt-1 hidden-sm-and-down"
-            contain
-            min-width="100"
-            src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-            width="100"
-        />
+<!--        <v-img-->
+<!--            alt="Vuetify Name"-->
+<!--            class="shrink mt-1 hidden-sm-and-down"-->
+<!--            contain-->
+<!--            min-width="100"-->
+<!--            src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"-->
+<!--            width="100"-->
+<!--        />-->
       </div>
 
       <v-spacer></v-spacer>
 
       <v-btn
-          href="https://github.com/vuetifyjs/vuetify/releases/latest"
+          href="https://github.com/danushka96/tubemate"
           target="_blank"
           text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <span class="mr-2">Latest</span>
+        <v-icon>mdi-github</v-icon>
       </v-btn>
     </v-app-bar>
 
